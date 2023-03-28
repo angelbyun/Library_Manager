edb2be516fa6a6729dc37ac5903e1777eea83b7f
Angel Byun <abyun22@turing.edu>
2023-03-28 14:55:37 -0600

    add user story 20

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,18 @@ Rails.application.routes.draw do
   post '/libraries', to: 'libraries#create'
   get '/libraries/:id/edit', to: 'libraries#edit'
   get '/libraries/:id', to: 'libraries#show'
+
   patch '/libraries/:id/edit', to: 'libraries#update'
   delete '/libraries/:id', to: 'libraries#destroy'
+
   get '/books', to: 'books#index'
   get '/libraries/:id/books/new', to: 'libraries/books#new'
   post '/libraries/:id/books/new', to: 'libraries/books#create'
   get '/books/:id', to: 'books#show'
   get '/books/:id/edit', to: 'books#edit'
+
   patch '/books/:id/edit', to: 'books#update'
+  delete '/books/:id', to: 'books#destroy'
+
   get '/libraries/:id/books', to: 'libraries/books#index'
 end
---
+++ spec/features/books/destroy_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'destroy a book' do
+  let!(:library_1) { Library.create!(name: 'Denver Public Library', city: 'Denver', opening_time: 10, closing_time: 18, book_available: false) }
+  let!(:library_2) { Library.create!(name: 'Douglas County Library', city: 'Highlands Ranch', opening_time: 8, closing_time: 20, book_available: true) }
+
+  let!(:book_1) { Book.create!(title: 'Harry Potter and the Sorcerers Stone', author: 'JK Rowling', fiction: true, number_of_copies: 2, library: library_2) }
+  let!(:book_2) { Book.create!(title: 'Verity', author: 'Colleen Hoover', fiction: true, number_of_copies: 0, library: library_1) }
+  let!(:book_3) { Book.create!(title: 'The Alchemist', author: 'Paulo Coelo', fiction: true, number_of_copies: 4, library: library_2) }
+  let!(:book_4) { Book.create!(title: 'Milk and Honey', author: 'Rupi Kaur', fiction: true, number_of_copies: 0, library: library_1) }
+
+  it 'can delete a book from show page' do
+    visit "/books/#{book_1.id}"
+
+    click_button("Delete Book")
+
+    expect(current_path).to eq("/books")
+    expect(page).to_not have_content(book_1.title)
+  end
+end
